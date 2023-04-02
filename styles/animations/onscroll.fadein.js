e9c12a83f566233722750a1fa9315987622594fb
unknown <francofyzz15@gmail.com>
2023-04-01 23:26:49 -0300

    Deleting the innecesary comment

--- styles/animations/onscroll.fadein.js
+++ styles/animations/onscroll.fadein.js
@@ -1,15 +1,12 @@
-const onscroll_fadein = () => {// Get all the elements you want to show on scroll
+const onscroll_fadein = () => {
     const targets = document.querySelectorAll(".js-show-on-scroll");
     const newOptions = {};
 
-    // Callback for IntersectionObserver
     function callback(entries) {
         entries.forEach(entry => {
             if (entry.isIntersecting) {
-            // Add the fadeIn class:
             entry.target.classList.add("motion-safe:animate-fadeIn");
             } else {
-            // Otherwise remove the fadein class
             entry.target.classList.remove("motion-safe:animate-fadeIn");
             }
         });
@@ -17,12 +14,9 @@ const onscroll_fadein = () => {// Get all the elements you want to show on scrol
 
     const observer = new IntersectionObserver(callback, newOptions);
 
-    // Loop through each of the target
     targets.forEach(function(target) {
-        // Hide the element
         target.classList.add("opacity-0");
 
-        // Add the element to the watcher
         observer.observe(target);
     });
 }
